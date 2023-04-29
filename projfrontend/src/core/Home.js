b8580550105367621dd2f64de5079929c1ca0f32
Sonali <sonalibyahut0125@gmail.com>
2023-04-29 21:13:45 +0530

    added slider cards and updated signin

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Base from './Base'
+import Slider from './Slider'
+
 export default function Home() {
   return (
     <div>
       <Base/>
-      
+      <Slider/>
     </div>
   )
 }
